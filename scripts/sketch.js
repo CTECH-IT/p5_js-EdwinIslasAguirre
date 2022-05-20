510231afa695cb897b095b958e4451950514a324
Edwin Islas Aguirre <8241001@isd535.org>
2022-05-20 11:27:10 -0500

    starting p5js project, halfway done

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,16 +1,31 @@
-let x = 0;
-
 function setup() {
-    let myCanvas = createCanvas(600, 400);
-    myCanvas.parent('myContainer');
+    createCanvas(500, 500);
+    background(275,240,225);
+    frameRate(2);
   }
 
 function draw() {
-    ellipse(x, height/2, 20, 20);
-    x = x + 1;
-
-function touchMoved() {
-    // do some stuff
-    return false;
+    drawFlower(random(width), random(height), random(10, 100));
 }
+
+function drawFlower(flowerX, flowerY, petalSize) {
+    let petalDistance = petalSize/ 2;
+
+    fill(255, 200, 200);
+
+    // upper-left petal
+    circle(flowerX - petalDistance, flowerY - petalDistance, petalSize);
+
+    // upper-right petal
+    circle(flowerX + petalDistance, flowerY - petalDistance, petalSize);
+
+    // lower-left petal
+    circle(flowerX - petalDistance, flowerY + petalDistance, petalSize);
+
+    // lower-right petal
+    circle(flowerX + petalDistance, flowerY + petalDistance, petalSize);
+
+    // center petal
+    fill(225, 0, 0);
+    circle(flowerX, flowerY, petalSize);
 }
