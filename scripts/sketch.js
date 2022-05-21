1279b799fe31ed485af2940114d5399a041e8f28
Edwin Islas Aguirre <8241001@isd535.org>
2022-05-21 01:20:20 -0500

    some final changes to my project?

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,11 +1,25 @@
-function setup() {
+ var fade;
+ var fadeAmount = 1;
+ 
+ function setup() {
     createCanvas(500, 500);
-    background(275,240,225);
-    frameRate(2);
-  }
+    background(275, 240, 225);
+    frameRate(0.5);
+    textSize(20);
+    fade = 0;
+}
 
 function draw() {
     drawFlower(random(width), random(height), random(10, 100));
+    text("What a strange thing!", 100, 100);
+    text("to be alive beneath cherry blossoms 🌸", 140, 170);
+    fill(0, 0, 0, fade);
+    if (fade<0) fadeAmount= 1;
+
+    if (fade>200) fadeAmount = -10;
+
+    fade += fadeAmount;
+    print(fade)
 }
 
 function drawFlower(flowerX, flowerY, petalSize) {
