2a2d58c841f3e8e2257d4f5a332b45288b2c5552
Edwin Islas Aguirre <8241001@isd535.org>
2022-05-23 00:10:36 -0500

    not sure what happened but everything is ok

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,45 +1,45 @@
- var fade;
- var fadeAmount = 1;
- 
- function setup() {
-    createCanvas(500, 500);
-    background(275, 240, 225);
-    frameRate(0.5);
-    textSize(20);
-    fade = 0;
+var fade;
+var fadeAmount = 1;
+
+function setup() {
+   createCanvas(500, 500);
+   background(275, 240, 225);
+   frameRate(0.5);
+   textSize(20);
+   fade = 0;
 }
 
 function draw() {
-    drawFlower(random(width), random(height), random(10, 100));
-    text("What a strange thing!", 100, 100);
-    text("to be alive beneath cherry blossoms 🌸", 140, 170);
-    fill(0, 0, 0, fade);
-    if (fade<0) fadeAmount= 1;
+   drawFlower(random(width), random(height), random(10, 100));
+   text("What a strange thing!", 100, 100);
+   text("to be alive beneath cherry blossoms 🌸", 140, 170);
+   fill(0, 0, 0, fade);
+   if (fade<0) fadeAmount= 1;
 
-    if (fade>200) fadeAmount = -10;
+   if (fade>200) fadeAmount = -10;
 
-    fade += fadeAmount;
-    print(fade)
+   fade += fadeAmount;
+   print(fade)
 }
 
 function drawFlower(flowerX, flowerY, petalSize) {
-    let petalDistance = petalSize/ 2;
+   let petalDistance = petalSize/ 2;
 
-    fill(255, 200, 200);
+   fill(255, 200, 200);
 
-    // upper-left petal
-    circle(flowerX - petalDistance, flowerY - petalDistance, petalSize);
+   // upper-left petal
+   circle(flowerX - petalDistance, flowerY - petalDistance, petalSize);
 
-    // upper-right petal
-    circle(flowerX + petalDistance, flowerY - petalDistance, petalSize);
+   // upper-right petal
+   circle(flowerX + petalDistance, flowerY - petalDistance, petalSize);
 
-    // lower-left petal
-    circle(flowerX - petalDistance, flowerY + petalDistance, petalSize);
+   // lower-left petal
+   circle(flowerX - petalDistance, flowerY + petalDistance, petalSize);
 
-    // lower-right petal
-    circle(flowerX + petalDistance, flowerY + petalDistance, petalSize);
+   // lower-right petal
+   circle(flowerX + petalDistance, flowerY + petalDistance, petalSize);
 
-    // center petal
-    fill(225, 0, 0);
-    circle(flowerX, flowerY, petalSize);
+   // center petal
+   fill(225, 0, 0);
+   circle(flowerX, flowerY, petalSize);
 }
